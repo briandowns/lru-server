62729d1e0ec233f53212d47fdd9149f5fd87e9ec
Brian Downs <brian.downs@gmail.com>
2018-09-20 17:24:29 -0700

    update

--- main.go
+++ main.go
@@ -2,47 +2,58 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/hashicorp/golang-lru"
 	"github.com/tidwall/modern-server"
 )
 
+const defaultSize = 1000000
+
 func main() {
 	var size int
 	var cache *lru.Cache
+	var err error
 	opts := &server.Options{
-		Version:     "0.0.1",
-		Name:        "lru-server",
-		Flags:       func() { flag.IntVar(&size, "s", 1000000, "") },
-		FlagsParsed: func() { cache, _ = lru.New(size) },
+		Version: "0.0.1",
+		Name:    "lru-server",
+		Flags:   func() { flag.IntVar(&size, "s", defaultSize, "") },
+		FlagsParsed: func() {
+			cache, err = lru.New(size)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				os.Exit(1)
+			}
+		},
 		Usage: func(s string) string {
 			return strings.Replace(s, "{{USAGE}}",
-				"  -s size      : size of lru (default: 1000000)\n", -1)
+				fmt.Sprintf("  -s size      : size of lru (default: %d)\n", defaultSize), -1)
 		},
 	}
 	server.Main(
 		func(w http.ResponseWriter, r *http.Request) {
 			key := strings.Split(r.URL.Path, "/")[1]
 			switch r.Method {
-			case "GET":
+			case http.MethodGet:
 				if val, ok := cache.Get(key); !ok {
-					w.WriteHeader(404)
+					w.WriteHeader(http.StatusNotFound)
 				} else {
 					w.Write([]byte(val.(string)))
 				}
-			case "DELETE":
+			case http.MethodDelete:
 				cache.Remove(key)
-			case "PUT", "POST":
+			case http.MethodPut, http.MethodPost:
 				if data, err := ioutil.ReadAll(r.Body); err != nil {
-					w.WriteHeader(500)
+					w.WriteHeader(http.StatusInternalServerError)
 				} else {
 					cache.Add(key, string(data))
 				}
 			default:
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 			}
 		}, opts,
 	)
